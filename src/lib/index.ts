aefe445d294f147eed2fb98fd261a710888446b1
Samit Koyom <samitkoyom@gmail.com>
2025-08-07 09:49:35 +0700

    update fixed

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 // Export everything from lib for easy imports
-export * from './components'
-export * from './layouts'
+// export * from './components' // Removed due to missing module
+// export * from './layouts' // Removed due to missing module
 export * from './services'
 export * from './stores'
 export * from './types'
